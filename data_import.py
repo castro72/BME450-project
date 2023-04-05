f1c90bfc2247710e7af7b4ac6f7eab17b48764c8
castro72 <125584380+castro72@users.noreply.github.com>
2023-04-05 19:01:02 -0400

    Update data_import.py

--- data_import.py
+++ data_import.py
@@ -1,28 +1,49 @@
-import numpy as np
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Apr  5 17:34:13 2023
+
+@author: jakecastro & Toby Miller
+"""
+import torch
 import pandas as pd
-from sklearn.tree import DecisionTreeClassifier
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import accuracy_score
-from xgboost import XGBClassifier
+pd.set_option('display.max_columns', None)
+import numpy as np
 import matplotlib.pyplot as plt
+#matplotlib inline
+import seaborn as sns
+pd.options.display.float_format = '{:.2f}'.format
+import warnings
+warnings.filterwarnings('ignore')
 
-df = pd.read_csv('/kaggle/input/heart-failure-prediction/heart.csv')
-df.head()
+## We want to import table
+df = pd.read_csv('/Users/jakecastro/Desktop/Classes/BME450/heart.csv')
 
-cat_variables = ['Sex',
-'ChestPainType',
-'RestingECG',
-'ExerciseAngina',
-'ST_Slope'
-]
+print(df.head())
 
-df = pd.get_dummies(data = df,
-                         prefix = cat_variables,
-                         columns = cat_variables)
-df.head()
+# Separate Results from inputs
 
-features = [x for x in df.columns if x not in 'HeartDisease']
-print(len(features))
+df_in = df.iloc[: , :-1] # data table without "Heart Disease" Column, so all the inputs
 
-df.head()
+df_results = df[["HeartDisease"]] # output [0] or [1] for Heart Disease
+
+print(df_in)
+print(df_results)
+
+#Create Training, Validation, and Test sets
+n1 = int(0.8 * int(len(df)))
+n2 = int(0.9 * int(len(df)))
+
+df_tr = df_in.iloc[:n1]
+df_val = df_in.iloc[n1:n2]
+df_tst = df_in.iloc[n2:]
+
+print(df_tr)
+print(df_val)
+print(df_tst)
+
+
+
+# print(df.iloc[:1])
+
+
